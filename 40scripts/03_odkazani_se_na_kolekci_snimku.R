9c327f5b2fb09b9b15a3c66534c07f7090282965
Ondrej Ledvinka <ondrej.ledvinka@chmi.cz>
2026-08-24 16:46:15 +0200

    another README typo corrected; comments added (or refined) to scripts 03 and 05

--- 40scripts/03_odkazani_se_na_kolekci_snimku.R
+++ 40scripts/03_odkazani_se_na_kolekci_snimku.R
@@ -8,4 +8,4 @@ ee_Initialize(user = "ledvinka@natur.cuni.cz")
 
 # odkážeme se na kolekci snímků (ImageCollection)
 # textové řetězce ve funkci lze kopírovat z Datového katalogu GEE, není třeba si je mapatovat
-coll <- ee$ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+col <- ee$ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
--- 40scripts/05_nahrani_vlastni_vektorove_vrstvy.R
+++ 40scripts/05_nahrani_vlastni_vektorove_vrstvy.R
@@ -8,29 +8,29 @@
 
 # načteme potřebné balíčky
 xfun::pkg_attach2(
-  "tidyverse",
-  "sf",
+  "tidyverse", # umoňuje zacházet s daty v tidyverse pojetí
+  "sf", # pro lokální práci s vektorovými vrstvami
   "rgee"
 )
 
 # inicializujeme připojení
 ee_Initialize(user = "ledvinka@natur.cuni.cz")
 
-# vytvořme nejprve lokálně bod reprezentující astronomický střed Evropy
+# vytvořme nejprve lokálně bod reprezentující např. astronomický střed Evropy
 stred <- st_point(c(15, 50)) |>
   st_sfc(crs = 4326) |>
   st_sf() |>
   st_set_geometry("geometry") |>
   mutate(
-    nm = "Kouřim",
+    name = "Kouřim",
     typ = "astronomický střed Evropy"
   )
 
 # funkce rgee::st_as_ee() je určena ke konverzi lokální geometrie, geometrického sloupce nebo vektorové vrstvy na GEE protějšky
 sf_as_ee(
   stred,
-  via = "getInfo_to_asset", # vrstva se nenahraje, pokud necháme getInfo (ta jen konvertuje do dočasného objektu)
-  assetId = "projects/ee-ledvinka/assets/astronomicky_stred_evropy" # je nutné znát cestu e složce s assety
+  via = "getInfo_to_asset", # vrstva se nenahraje, pokud necháme možnost "getInfo" (ta jen konvertuje do dočasného objektu)
+  assetId = "projects/ee-ledvinka/assets/astronomicky_stred_evropy" # je nutné znát cestu ke složce s assety
 )
 
 # v současné verzi rgee je možné se setkat s chybou, ale vrstva se nahraje, o čemž je možné se snadno přesvědčit
