78eb08caf1c89a8c8d408697a0539174fb0dc53b
Ondrej Ledvinka <ledvinka@natur.cuni.cz>
2026-09-09 09:52:18 +0200

    one more script about filtering added strategically after the other scripts about filtering; therefore, the names of scripts that were already ready were changed accordingly

---
+++ 40scripts/19_funkce_ee_Filter_calendarRange.R
@@ -0,0 +1,30 @@
+# Filtr kolekcí zohledňující pravidelnost období -------------------------
+
+# při různých aplikacích narážíme na problém výběru prvků kolekcí za stejné období napříč více roky
+# řekněme, že nás např. zajímají optické snímky jenom každého měsíce července
+# takové výběry se mohou hodit např. v lesnictví nebo zemědělství
+# tento výběr (filtr) je usnadněn serverovou funkcí ee$Filter$calendarRange()
+
+# načteme balíček rgee
+xfun::pkg_attach2("rgee")
+
+# zahájíme připojení
+ee_Initialize(
+  user = "ledvinka@natur.cuni.cz"
+)
+
+# napojíme se např. na kolekci snímků mise Sentinel-2
+col <-
+  ee$ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+
+# zkusme se zaměřit na července posledních pěti let
+# a zároveň se zaměříme na snímky zahrnující astronomický střed Evropy
+col2 <-
+  col$filterDate("2022-07-01", "2026-08-01")$filterBounds(ee$Geometry$Point(
+    15,
+    50
+  ))$filter(ee$Filter$calendarRange(7, 7, "month"))
+
+# podmínky splňuje 150 snímků
+# jak se mmůžeme snadno přesvědčit následujícím způsobem
+col2$size()$getInfo()
