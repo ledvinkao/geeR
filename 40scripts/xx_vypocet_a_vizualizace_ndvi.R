2af1d1b1d8a55ce5bea3d3a3cf552057e93bb13b
Ondrej Ledvinka <ondrej.ledvinka@chmi.cz>
2026-09-10 10:52:50 +0200

    script for NDVI computation and its visualization added

---
+++ 40scripts/xx_vypocet_a_vizualizace_ndvi.R
@@ -0,0 +1,71 @@
+# Výpočet a vizualizace NDVI ---------------------------------------------
+
+# NDVI je snad nejužívanějším spektrálním (vegetačním) indexem, který se v dálkovém průzkumu Země odvozuje z optických dat
+# z toho důvodu také v GEE existuje funkce normalizedDifference(), který však obecně dokáže vypočítat i jiné indexy založené na normalizovaném rozdílu hodnot dvou pásem
+
+# načteme balíček rgee
+# k tomu pro demonstraci uklizených kolekcí snímků načteme i balíček tidyrgee a tidyverse
+# může se to hodit při hledání dlaždic bezoblačného dne při mozaikování
+xfun::pkg_attach2(
+  "tidyverse",
+  "rgee",
+  "tidyrgee"
+)
+
+# zahájíme připojení
+ee_Initialize(
+  user = "ledvinka@natur.cuni.cz",
+  drive = T # to jen, pokud očekáváme, že transfer výsledků půjde přes Google Drive
+)
+
+# demonstrujme výsledek na snímcích mise Sentinel-2
+# konkrétně se podíváme na měsíc září 2024, kdy se v Česku a okolí odehrály významné povodně
+# zaměříme se na bod reprezentující suchý poldr Wlodzienin v Polsku a jeho 30km okolí
+# duvodem je zaměření se na snímky poblíž Krnovska, které bylo povodní velmi zasaženo
+col <- 
+  ee$ImageCollection("COPERNICUS/S2_SR_HARMONIZED")$
+  filterDate("2024-09-01", "2024-10-01")$
+  filterBounds(ee$Geometry$Point(17.8276967, 50.1168703)$buffer(30000))$
+  filter(ee$Filter$lt("CLOUDY_PIXEL_PERCENTAGE", 10))
+
+# prohlédneme kolekci jako uklizený objekt, ať máme lepší povědomí o datumech
+meta <- tidyrgee::as_tidyee(col)
+
+# klidně si ještě odhnízdíme sloupec 'band_names', ať vidíme označení pásem
+# musíme se dostat do tabulky v seznamu s názvem 'vrt'
+meta |> 
+  pluck("vrt") |> 
+  unnest(band_names)
+
+# vidíme, že v omezené kolekci je hned několik dnů na výběr
+# podívejme se např. na 18. září a všechny dostupné dlaždice mozaikujme
+img <- 
+  col <- 
+  ee$ImageCollection("COPERNICUS/S2_SR_HARMONIZED")$
+  filterDate("2024-09-18", "2026-09-19")$
+  filterBounds(ee$Geometry$Point(17.8276967, 50.1168703)$buffer(30000))$
+  mosaic()
+
+# nyní můžeme pro takto vzniklý snímek aplikovat výpočet NDVI
+# bez vnořené klientské funkce list() serverová funkce normalizedDifference() nefungovala, tak funkci list() vkládáme
+# musíme si dát pozor s jakými produkty zrovna pracujeme, protože pro každou družicovou misi máme pásma uspořádaná jinak
+# pro Sentinel-2 tedy platí, že červené pásmo viditelného spektra je reprezentováno pásmem B4
+# blízké infračervené pásmo (NIR) je reprezentováno pásmem B8
+ndvi <- img$normalizedDifference(list("B8", "B4"))
+
+# před kreslením centrujeme na bod zájmu a použijeme např. úroveň zoomu 11
+Map$centerObject(ee$Geometry$Point(17.8276967, 50.1168703), 11)
+
+# a kreslíme (díky možnostem R balíčku balíčku mapview lze takto dynamicky)
+# speciálně se ve funkci Map$addLayer() musíme postarat o správnou paletu barev
+# vektor hexadecimálních kódů barev např. dostaneme od AI:-)
+# posledním argumentem při kreslení je název vrstvy
+Map$addLayer(
+    ndvi,
+    list(
+      min = -1,
+      max = 1,
+      palette = c("#8c510a", "#d8b365", "#f6e8c3", "#c7eaea", "#80cdc1", "#01665e")
+    ),
+    'NDVI'
+  )
